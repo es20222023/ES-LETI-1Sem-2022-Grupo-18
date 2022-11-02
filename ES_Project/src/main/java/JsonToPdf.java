3f55aa0208ad8660825b5241a21786a0f9956d31
Madalena <mmrtj@iscte-iul.pt>
2022-11-02 11:43:34 +0000

    Adição das Metricas & Testes diferentes

--- ES_Project/src/main/java/JsonToPdf.java
+++ ES_Project/src/main/java/JsonToPdf.java
@@ -14,8 +14,10 @@ public class JsonToPdf {
 	static public void testes(String content) {
 
 		// read JSON file
+		
 		//String fileName = "formatted.json";
 		//String content = "{'id':'331','nome':'Produto 1', valor':'11.98'},{'id':'331','nome':'Produto 2', valor':'11.98'},{'id':'331','nome':'Produto 3', valor':'11.98'}";
+		
 		// create a blank Workbook object
 		Workbook workbook = new Workbook();
 		// access default empty worksheet
@@ -26,14 +28,12 @@ public class JsonToPdf {
 		//String fileName = "formatted.json";
 			try {
 				//content = new String(Files.readAllBytes(Paths.get(fileName)));
-
-
-
+				
 			//set JsonLayoutOptions for formatting
 			JsonLayoutOptions l = new JsonLayoutOptions();
 			l.setArrayAsTable(true);
-			//import JSON data to default worksheet starting at cell A1
 			
+			//import JSON data to default worksheet starting at cell A1	
 				JsonUtility.importData(content, worksheet.getCells(), 0, 0, l);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -46,7 +46,7 @@ public class JsonToPdf {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("feito");
+			System.out.println("O PDF foi gerado");
 			}
 		}
 
--- ES_Project/src/main/java/Main.java
+++ ES_Project/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String [] args) {
 		System.out.println("123");
-		String a = "Testar";
+		String a = "{'id':'331','nome':'Produto 1', valor':'11.98'},{'id':'331','nome':'Produto 2', valor':'11.98'},{'id':'331','nome':'Produto 3', valor':'11.98'}";
 		JsonToPdf.testes(a);
 	}
 }
