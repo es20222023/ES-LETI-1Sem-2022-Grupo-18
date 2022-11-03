386b43296bfac288c030b7b748c7eb665e4c20c3
andre <andre@ASUS-ANDRE.home>
2022-11-03 00:49:54 +0000

    Adding Test Classes And Metrics

--- ES_Project/src/test/java/JsonToPdfTest.java
+++ ES_Project/src/test/java/JsonToPdfTest.java
@@ -5,24 +5,25 @@ import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+import com.aspose.cells.Workbook;
+
+class JsonToPdfTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-			assertTrue(true);
-			assertFalse(false);
-			assertEquals(10,10);
-			boolean [] b1 = {true,true};
-			boolean [] b2 = {true,true};
-			assertArrayEquals(b1, b2);
-		}
-		
+		assertTrue(true);
+		assertFalse(false);
+		assertEquals(10,10);
+	}
+	
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 	private final PrintStream originalOut = System.out;
@@ -39,22 +40,26 @@ class MainTest {
 	    System.setOut(originalOut);
 	    System.setErr(originalErr);
 	}
-
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	private final PrintStream standardOut = System.out;
+	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+	@BeforeEach
+	public void setUp() {
+	    System.setOut(new PrintStream(outputStreamCaptor));
+	}
+	
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void testTestes() {
+		
 	}
-
+	
+	
 }
