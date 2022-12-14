9378bcb81b9f053ecf9c94a9477c220f4cc4a68f
AlexisMont01 <acmoa3@iscte-iul.pt>
2022-12-14 18:32:30 +0000

    Created FrameDashboard and ImportPanel

--- ES_Project/src/main/java/FrameDashboard.java
+++ ES_Project/src/main/java/FrameDashboard.java
@@ -1,5 +1,7 @@
 import java.awt.EventQueue;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -7,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
 import javax.swing.JToolBar;
 import javax.swing.JTextField;
@@ -18,6 +21,10 @@ public class FrameDashboard extends JFrame {
 	private JPanel backgroundPane;
 	private Image IconLogo = new ImageIcon(FrameLogin.class.getResource("Res/logo.png")).getImage().getScaledInstance(150,150,Image.SCALE_SMOOTH);
 	private Image home = new ImageIcon(FrameLogin.class.getResource("Res/home.png")).getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH);
+	private PanelHome PanelHome2;
+	private ImportPanel ImportPanel2;
+	private ViewPanel ViewPanel2;
+	private ExportPanel ExportPanel2;
 	/**
 	 * Launch the application.
 	 */
@@ -39,8 +46,9 @@ public class FrameDashboard extends JFrame {
 	 */
 	public FrameDashboard() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		setBounds(100, 100, 807, 550);
-		setUndecorated(true);
+		setUndecorated(false);
 		backgroundPane = new JPanel();
 		backgroundPane.setBackground(new Color(255, 255, 255));
 		backgroundPane.setBorder(new LineBorder(new Color(0, 128, 128)));
@@ -48,13 +56,27 @@ public class FrameDashboard extends JFrame {
 		setContentPane(backgroundPane);
 		backgroundPane.setLayout(null);
 		
+		PanelHome2 = new PanelHome();
+		ImportPanel2= new ImportPanel();
+		ViewPanel2 = new ViewPanel();
+		ExportPanel2 = new ExportPanel();
+		
+		//Menu Panel
 		JPanel MenuPanel = new JPanel();
 		MenuPanel.setBounds(0, 160, 219, 390);
 		MenuPanel.setBackground(new Color(0, 128, 128));
 		backgroundPane.add(MenuPanel);
 		MenuPanel.setLayout(null);
 		
+		// Home Button
 		JPanel panelHome = new JPanel();
+		//panelHome.addMouseListener(new PanelButtomMouseAdapter(PanelHome2));
+		panelHome.addMouseListener(new PanelButtomMouseAdapter(panelHome) {
+			@Override
+			public void mouseClicked(MouseEvent e){
+				menuClicked(PanelHome2);
+			}
+		});
 		panelHome.setBorder(new LineBorder(new Color(255, 255, 255)));
 		panelHome.setForeground(new Color(255, 255, 255));
 		panelHome.setToolTipText("");
@@ -63,13 +85,21 @@ public class FrameDashboard extends JFrame {
 		MenuPanel.add(panelHome);
 		panelHome.setLayout(null);
 		
+		// Text Home Button
 		JLabel lblLabelHome = new JLabel("Home");
 		lblLabelHome.setBounds(83, 6, 65, 35);
 		lblLabelHome.setForeground(new Color(255, 255, 255));
 		lblLabelHome.setFont(new Font("Dialog", Font.PLAIN, 16));
 		panelHome.add(lblLabelHome);
 		
+		// Import Button
 		JPanel ImportPanel = new JPanel();
+		ImportPanel.addMouseListener(new PanelButtomMouseAdapter(ImportPanel) {
+			@Override
+			public void mouseClicked(MouseEvent e){
+				menuClicked(ImportPanel2);
+			}
+		});
 		ImportPanel.setBorder(new LineBorder(new Color(255, 255, 255)));
 		ImportPanel.setToolTipText("");
 		ImportPanel.setForeground(Color.WHITE);
@@ -78,6 +108,7 @@ public class FrameDashboard extends JFrame {
 		MenuPanel.add(ImportPanel);
 		ImportPanel.setLayout(null);
 		
+		// Text Import Button
 		JLabel lblNewLabelImport = new JLabel("Import");
 		lblNewLabelImport.setForeground(Color.WHITE);
 		lblNewLabelImport.setFont(new Font("Dialog", Font.PLAIN, 16));
@@ -85,6 +116,12 @@ public class FrameDashboard extends JFrame {
 		ImportPanel.add(lblNewLabelImport);
 		
 		JPanel ViewPanel = new JPanel();
+		ViewPanel.addMouseListener(new PanelButtomMouseAdapter(ViewPanel) {
+			@Override
+			public void mouseClicked(MouseEvent e){
+				menuClicked(ViewPanel2);
+			}
+		});
 		ViewPanel.setBorder(new LineBorder(new Color(255, 255, 255)));
 		ViewPanel.setToolTipText("");
 		ViewPanel.setForeground(Color.WHITE);
@@ -100,6 +137,12 @@ public class FrameDashboard extends JFrame {
 		ViewPanel.add(lblViewCalendars);
 		
 		JPanel ExportPanel = new JPanel();
+		ExportPanel.addMouseListener(new PanelButtomMouseAdapter(ExportPanel) {
+			@Override
+			public void mouseClicked(MouseEvent e){
+				menuClicked(ExportPanel2);
+			}
+		});
 		ExportPanel.setBorder(new LineBorder(new Color(255, 255, 255)));
 		ExportPanel.setToolTipText("");
 		ExportPanel.setForeground(Color.WHITE);
@@ -114,18 +157,90 @@ public class FrameDashboard extends JFrame {
 		lblExport.setBounds(72, 6, 65, 35);
 		ExportPanel.add(lblExport);
 		
-		JPanel ToDoPanel = new JPanel();
-		ToDoPanel.setBorder(new LineBorder(new Color(255, 255, 255)));
-		ToDoPanel.setToolTipText("");
-		ToDoPanel.setForeground(Color.WHITE);
-		ToDoPanel.setBackground(new Color(0, 128, 128));
-		ToDoPanel.setBounds(0, 218, 219, 47);
-		MenuPanel.add(ToDoPanel);
-		ToDoPanel.setLayout(null);
+		JPanel signOutPane = new JPanel();
+		signOutPane.addMouseListener(new PanelButtomMouseAdapter(signOutPane) {
+			@Override
+			public void mouseClicked(MouseEvent e){
+				if(JOptionPane.showConfirmDialog(null, "Are you sure that you want to sign out?") == 0) {
+					FrameLogin frameLogin = new FrameLogin();
+					frameLogin.setVisible(true);
+					FrameDashboard.this.setVisible(false);
+					FrameDashboard.this.dispose();
+				}
+				
+			}
+		});
+		signOutPane.setBorder(new LineBorder(new Color(255, 255, 255)));
+		signOutPane.setToolTipText("");
+		signOutPane.setForeground(Color.WHITE);
+		signOutPane.setBackground(new Color(0, 128, 128));
+		signOutPane.setBounds(0, 218, 219, 47);
+		MenuPanel.add(signOutPane);
+		signOutPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("SIGN OUT");
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(63, 6, 83, 35);
+		signOutPane.add(lblNewLabel);
 		
 		JLabel lblLogoLabel = new JLabel("");
 		lblLogoLabel.setBounds(22, 17, 174, 135);
 		backgroundPane.add(lblLogoLabel);
 		lblLogoLabel.setIcon(new ImageIcon(IconLogo));
+		
+		JPanel panelMain = new JPanel();
+		panelMain.setBackground(new Color(255, 255, 255));
+		panelMain.setBounds(235, 67, 551, 433);
+		backgroundPane.add(panelMain);
+		panelMain.setLayout(null);
+		
+		panelMain.add(PanelHome2);
+		panelMain.add(ImportPanel2);
+		panelMain.add(ExportPanel2);
+		panelMain.add(ViewPanel2);
+		
+		menuClicked(PanelHome2);
+	}
+	
+	public void menuClicked(JPanel panel) {
+		PanelHome2.setVisible(false);
+		ImportPanel2.setVisible(false);
+		ViewPanel2.setVisible(false);
+		ExportPanel2.setVisible(false);
+		
+		panel.setVisible(true);
+		
+	}
+	
+	private class PanelButtomMouseAdapter extends MouseAdapter{
+		JPanel panel;
+		public PanelButtomMouseAdapter(JPanel panel) {
+			this.panel = panel;
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			panel.setForeground(new Color(112, 128, 144));
+		
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			panel.setForeground(new Color(47, 79, 79));
+			
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			panel.setForeground(new Color(60, 179, 113));
+			
+			
+		}
+		
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			panel.setForeground(new Color(112, 128, 144));
+			
+		}
 	}
 }
---
+++ ES_Project/src/main/java/ViewPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+public class ViewPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ViewPanel() {
+		setSize(551,433);
+
+	}
+
+}
