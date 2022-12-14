41a9d7855ef28da45f02d715d109350e4cea28d9
andrejcoliveira18 <andre@ASUS-ANDRE.home>
2022-12-14 00:59:52 +0000

    Tests

---
+++ ES_Project/src/test/java/BarChartTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BarChartTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		assertTrue(true);
+		assertFalse(false);
+		boolean [] arr1 = new boolean[]{true, true};
+		boolean [] arr2 = new boolean[]{true, true};
+		assertEquals(arr1,arr2);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
