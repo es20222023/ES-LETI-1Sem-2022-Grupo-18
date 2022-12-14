b6ff84d5e629197a7e0b2561d3206e3778057676
andrejcoliveira18 <andre@ASUS-ANDRE.home>
2022-12-14 01:11:28 +0000

    Tests

---
+++ ES_Project/src/test/java/MainTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MainTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		assertTrue(true);
+		assertFalse(false);
+		assertEquals(10,10);
+		boolean [] b1 = {true,true};
+		boolean [] b2 = {true,true};
+		
+		//assertArrayEquals(b1, b2);
+	
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
